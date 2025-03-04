caadab32536e59386971b69fc8ff91417002b168
Fawwaz Ashraf <fashraf@smu.edu>
2025-03-03 23:48:57 -0600

    added hw2

---
+++ hw2/vec2d_b.cpp
@@ -0,0 +1,259 @@
+/* Daniel R. Reynolds
+   SMU Mathematics
+   Math 4370 / 6370 */
+
+// Inclusions
+#include "vec2d_b.hpp"
+
+
+// This file implements the operations defined in the vec2d class
+
+// Helper function for accessing a 2d index in a flattened array
+long int idx(long int m, long int i, long int j) {
+  return m * i + j;
+}
+
+///// General class routines /////
+
+// constructor (initializes values to 0.0)
+vec2d::vec2d(long int m, long int n) {
+  // if len is illegal, create an empty vector
+  if (m < 1 || n < 1) {
+    this->m = 0;
+    this->n = 0;
+    this->data = NULL;
+  } else {
+    this->m = m;
+    this->n = n;
+    data = new double[m * n];
+    for (long int i=0; i< m * n; i++) {
+      data[i] = 0.0;
+    }
+  }
+}
+
+
+// destructor (frees space associated with a vec2d)
+vec2d::~vec2d() {
+  if (this->data!=NULL)
+    delete[] this->data;
+  this->m = 0;
+  this->n = 0;
+}
+
+
+// write myself to stdout
+void vec2d::Write() const {
+  // throw exception if data array isn't allocated
+  if (this->data == NULL)
+    throw std::invalid_argument( "vec2d::Write error, empty data array" );
+
+  // print data to screen
+  for (long int i=0; i<this->m; i++) {
+    for (long int j=0; j<this->n; j++) {
+      std::cout << std::setprecision(17) << "  " << this->data[idx(m, i, j)];
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl;
+}
+
+
+// write myself to a file
+void vec2d::Write(const char *outfile) const {
+  // throw exception if data array isn't allocated
+  if (this->data == NULL)
+    throw std::invalid_argument( "vec2d::Write error, empty data array" );
+
+  // throw exception if 'outfile' is empty
+  if (strlen(outfile) < 1)
+    throw std::invalid_argument( "vec2d::Write error, empty outfile" );
+
+  // open output file
+  std::ofstream fstr;
+  fstr.open(outfile);
+  if (!fstr.is_open())
+    throw std::invalid_argument( "vec2d::Write error, unable to open file for writing" );
+
+  // print data to file
+  for (long int i=0; i<this->m; i++) {
+    for (long int j=0; j<this->n; j++) {
+      fstr << std::setprecision(17) << "  " << this->data[idx(m, i, j)];
+    }
+    fstr << std::endl;
+  }
+  fstr << std::endl;
+
+  // close output file
+  fstr.close();
+}
+
+///// Arithmetic operations defined on a given vec2d /////
+
+// x = a*y + b*z
+void vec2d::LinearSum(double a, const vec2d& y, double b, const vec2d& z) {
+  // check that array sizes match
+  if (y.m != this->m  ||  z.m != this->m || y.n != this->n  ||  z.n != this->n)
+    throw std::invalid_argument( "vec2d::LinearSum error, vector sizes do not match" );
+
+  // check that data is not NULL
+  if (this->data == NULL || y.data == NULL || z.data == NULL)
+    throw std::invalid_argument( "vec2d::LinearSum error: empty data array" );
+
+  // perform operation
+  for (long int i=0; i<this->m * this->n; i++) {
+    this->data[i] = a*y.data[i] + b*z.data[i];
+  }
+}
+
+
+//   x = x*a  (scales my data by scalar a)
+void vec2d::Scale(double a) {
+  // check that data is not NULL
+  if (this->data == NULL)
+    throw std::invalid_argument( "vec2d::Scale error: empty data array" );
+
+  // perform operation
+  for (long int i=0; i < this->m * this->n; i++) {
+    this->data[i] *= a;
+  }
+}
+
+
+//   x = y  (copies y into x)
+void vec2d::Copy(const vec2d& y) {
+  // check that array sizes match
+  if (y.m != this->m ||y.n != this->n)
+    throw std::invalid_argument( "vec2d::Copy error, vector sizes do not match" );
+
+  // check that data is not NULL
+  if (this->data == NULL || y.data == NULL)
+    throw std::invalid_argument( "vec2d::Copy error: empty data array" );
+
+  // perform operation
+  for (long int i=0; i<this->m * this->n; i++) {
+    this->data[i] = y.data[i];
+  }
+}
+
+
+//   x = a  (sets all entries of x to the scalar a)
+void vec2d::Constant(double a) {
+  // check that data is not NULL
+  if (this->data == NULL)
+    throw std::invalid_argument( "vec2d::Copy error: empty data array" );
+
+  // perform operation and return
+  for (long int i=0; i<this->m * this->n; i++) {
+    this->data[i] = a;
+  }
+}
+
+
+///// scalar quantities derived from vectors /////
+
+// min x_i
+double vec2d::Min() {
+  // check that my data is allocated
+  if (this->data == NULL)
+    throw std::invalid_argument( "vec2d::Min error: empty data array" );
+
+  // perform operation and return
+  double mn = this->data[0];
+  for (long int i=0; i<this->m * this->n; i++) {
+    mn = std::min(mn, this->data[i]);
+  }    
+  return mn;
+}
+
+
+// max x_i
+double vec2d::Max() {
+  // check that my data is allocated
+  if (this->data == NULL)
+    throw std::invalid_argument( "vec2d::Max error: empty data array" );
+
+  // perform operation and return
+  double mx = this->data[0];
+  for (long int i=0; i<this->m * this->n; i++) {
+    mx = std::max(mx, this->data[i]);
+  }   
+
+  return mx;
+}
+
+
+
+///// independent constructor routines /////
+
+// create a vector of linearly spaced data
+vec2d Linspace(double a, double b, long int m, long int n) {
+  vec2d *x = new vec2d(m, n);
+  double *xd = x->GetData();
+  double h = (b-a)/(n * m -1);
+  for (long int i=0; i<m * n; i++) {
+    xd[i] = a + h*i;
+  }  
+  return *x;
+}
+
+
+// create a vector of uniformly-distributed random data
+vec2d Random(long int m, long int n) {
+  vec2d *x = new vec2d(m, n);
+  double *xd = x->GetData();
+  for (long int i=0; i<m * n; i++) {
+    xd[i] = random() / (pow(2.0,31.0) - 1.0);
+  }
+  return *x;
+}
+
+
+///// independent arithmetic routines /////
+
+// dot-product of x and y
+double Dot(const vec2d& x, const vec2d& y) {
+  // check that array sizes match
+  if (y.M() != x.M() || y.N() != x.N())
+    throw std::invalid_argument( "vec2d::Dot error, vector sizes do not match" );
+
+  // perform operation and return
+  double sum = 0.0;
+  for (long int i=0; i<y.M() * y.N(); i++) {
+    sum += x[i]*y[i];
+  }
+  return sum;
+}
+
+
+
+// ||x||_2
+double TwoNorm(const vec2d& x) {
+  double sum = 0.0;
+  for (long int i=0; i<x.M() * x.N(); i++) {
+      sum += x[i]*x[i];
+  }
+  
+  return sqrt(sum);
+}
+
+
+// ||x||_RMS
+double RmsNorm(const vec2d& x) {
+  double sum = 0.0;
+  for (long int i=0; i<x.M() * x.N(); i++) {
+    sum += x[i]*x[i];
+  }
+  return sqrt(sum/x.Size());
+}
+
+
+// ||x||_infty
+double MaxNorm(const vec2d& x) {
+  double mx = 0.0;
+  for (long int i=0; i<x.M() * x.N(); i++) {
+    mx = std::max(mx, std::abs(x[i]));
+  }
+  return mx;
+}
---
+++ hw2/vec2d_test.cpp
@@ -0,0 +1,143 @@
+/* Daniel R. Reynolds
+   SMU Mathematics
+   Math 4370 / 6370 */
+
+// Inclusions
+#include <stdlib.h>
+#include <chrono>
+#include <iostream>
+#include "vec2d.hpp"
+
+// prototype for Gram-Schmidt routine
+int GramSchmidt2d(vec2d X[], int numvectors);
+
+// Example routine to test the vec1d class
+int main(int argc, char* argv[]) {
+
+  // create some vecs of size 5x5, and set some entries
+  vec2d a(5, 5), b(5, 5), c(5, 5);
+  for (int i=0; i<5; i++) for (int j=0; j<5; j++) b[i][j] = (i*5+j+1)*0.1;
+  for (int i=0; i<5; i++) for (int j=0; j<5; j++) c[i][j] = (i*5+j+1);
+
+  // output to screen
+  std::cout << "writing 2d array of zeros" << std::endl;
+  a.Write();
+  std::cout << "writing 2d array of 0.1, 0.2, ..., 2.4, 2.5:" << std::endl;
+  b.Write();
+  std::cout << "writing 2d array of 1,2,...,24,25:" << std::endl;
+  c.Write();
+
+  // verify that b has a size of 25
+  if (b.Size() != 25)
+    std::cerr << "error: incorrect vector length" << std::endl;
+
+  // access a's data array, update entries, and write to file
+  double **dat = a.GetData();
+  dat[0][0] = 10.0;
+  dat[1][1] = 15.0;
+  dat[2][2] = 20.0;
+  dat[3][3] = 25.0;
+  dat[4][4] = 30.0;
+  a.Write("a_data");
+  std::cout << "the new file 'a_data' on disk should have entries 10, 15, 20, 25, 30 on the diagonal and zeroes elsewhere" << std::endl << std::endl;
+
+  // access each entry of a and write to screen
+  std::cout << "entries of a, one at a time (via []): should give 10, 15, 20, 25, 30" << std::endl;
+  for (int i=0; i<5; i++)
+    std::cout << "  " << a[i][i] << std::endl;
+  std::cout << std::endl;
+
+  // update one entry of a
+  std::cout << "updating the last entry of a to be 31 (via a[4]):" << std::endl;
+  a[4][4] = 31.0;
+  a.Write();
+  a[4][4] = 30.0;  // reset to original
+
+  // Test arithmetic operators
+  std::cout << "Testing vector constant, should all be -1" << std::endl;
+  b.Constant(-1.0);
+  b.Write();
+
+  std::cout << "Testing vector copy, should be 1, 2, ..., 24, 25" << std::endl;
+  a.Copy(c);
+  a.Write();
+
+  std::cout << "Testing scalar multiply, should be 5, 10, ..., 120, 125" << std::endl;
+  c.Scale(5.0);
+  c.Write();
+
+  // create a few vecs of size 10x10
+  vec2d X[] = {vec2d(10, 10), vec2d(10, 10), vec2d(10, 10), vec2d(10, 10), vec2d(10, 10)};
+
+  // fill in the vectors
+  for (int i=0; i<10; i++) {
+    for (int j=0; j<10; j++) {
+      X[0][i][j] = 1.0*(i + j);
+      X[1][i][j] = -5.0 + 1.0*(i + j);
+      X[2][i][j] = 2.0 + 2.0*(i + j);
+      X[3][i][j] = 20.0 - 1.0*(i + j);
+      X[4][i][j] = -20.0 + 1.0*(i + j);
+    }
+  }
+
+  // check the LinearSum routine
+  X[0].LinearSum(-2.0, X[1], 1.0, X[2]);
+  std::cout << "Testing LinearSum, should be all 12:" << std::endl;
+  X[0].Write();
+
+  // check the various scalar output routines
+  std::cout << "Testing TwoNorm:  " << TwoNorm(b) << std::endl;
+
+  std::cout << "Testing RmsNorm:  " << RmsNorm(c) << std::endl;
+
+  std::cout << "Testing MaxNorm (should be 1):  " << MaxNorm(b) << std::endl;
+
+  std::cout << "Testing Min (should be 1):  " << a.Min() << std::endl;
+
+  std::cout << "Testing Max (should be 125):  " << c.Max() << std::endl;
+
+  std::cout << "Testing Dot:  " << Dot(a, c) << std::endl;
+  std::cout << "Testing Linspace, should be 0 1 ... 23 24" << std::endl;
+  vec2d d = Linspace(0.0, 24.0, 5, 5);
+  d.Write();
+
+  std::cout << "Testing Random" << std::endl;
+  vec2d f = Random(5, 5);
+  f.Write();
+
+  /// performance/validity tests (Gram-Schmidt process)
+  std::pair<long int, long int> tests[] = {{10000, 1000}, {1000, 10000}, {100, 100000}, {10, 1000000}, {100000, 100}, {1000000, 10}};
+  std::cout << "Running GramSchmidt2d processes" << std::endl;
+  for(size_t q = 0; q < 6; q++) {
+    long int m = tests[q].first;
+    long int n = tests[q].second;
+    vec2d Y[] = {Random(m, n), Random(m, n), Random(m, n), Random(m, n), Random(m, n)};
+    std::chrono::time_point<std::chrono::system_clock> stime = std::chrono::system_clock::now();
+    if (GramSchmidt2d(Y,5))
+      std::cerr << "GramSchmidt2d returned error" << std::endl;
+    std::chrono::time_point<std::chrono::system_clock> ftime = std::chrono::system_clock::now();
+    std::chrono::duration<double> rtime = ftime-stime;
+    std::cout << "  GramSchmidt2d time (m =" << m << ",n=" << n << "): " << rtime.count() << std::endl << std::endl;
+
+    std::cout << "Resulting vectors should be orthonormal:" << std::endl;
+    bool pass = true;
+    double tolerance = 1e-12;
+    for (int i=0; i<5; i++) {
+      if (std::abs(Dot(Y[i], Y[i]) - 1.0) > tolerance) {
+        pass = false;
+        std::cout << "  <Y[" << i << "],Y[" << i << "]> = " << Dot(Y[i], Y[i]) << std::endl;
+      }
+      for (int j=i+1; j<5; j++)
+        if (std::abs(Dot(Y[i], Y[j])) > tolerance) {
+          pass = false;
+          std::cout << "  <Y[" << i << "],Y[" << j << "]> = " << Dot(Y[i], Y[j]) << std::endl;
+        }
+    }
+    if (pass)
+      std::cout << "  passed orthonormality check" << std::endl << std::endl;
+    else
+      std::cout << "  failed orthonormality check" << std::endl << std::endl;
+
+  }
+  return 0;
+} // end main
